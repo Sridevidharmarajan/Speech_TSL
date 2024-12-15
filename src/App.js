7aa80703bbbe8765c951b0b1729b858002cf218e
Sidevidharmarajan <sridevidharmarajan18@example.com>
2024-12-15 09:51:39 +0530

    Commit changes with line-ending conversion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SpeechRecognitionComponent from "./SpeechRecognition";
+import Login from "./Login";
+import Signup from "./Signup";
+1
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container mt-5">
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/main" element={<SpeechRecognitionComponent />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
